0d1a45b839285bfea9b30e2ba7358569929d1486
Kelechi Ukpabi <kuolingou@gmail.com>
2024-09-07 14:00:59 -0400

    Update pokecli.js

--- pokecli.js
+++ pokecli.js
@@ -7,18 +7,11 @@ import { getColorFromURL } from 'color-thief-node';
 
 
 //DISPLAY TITLE AT THE START
-art.font('POKECLI', 'Doom', { width: 80 }, (err, rendered) => {
+art.font('POKECLI', 'Doom', (err, rendered) => {
   if (err) {
     console.error('Error generating ASCII art:', err);
   } else {
-    const lines = rendered.split('\n');
-    lines.forEach(line => {
-      const middle = Math.floor(line.length / 2);
-      const firstHalf = line.slice(0, middle);  
-      const secondHalf = line.slice(middle);
-      
-      
-      console.log(chalk.redBright(firstHalf) + chalk.whiteBright(secondHalf));
+    console.log(chalk.redBright(rendered));
     }) 
   }
   
